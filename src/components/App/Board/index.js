4dd0502fb1a7d0101917dde084f6b513b0c8b8d2
Vianney Carel <vianney.carel@gmail.com>
2017-11-12 22:50:49 +0100

    Add keyboard event handler to mode the tetrominos

--- src/components/App/Board/index.js
+++ src/components/App/Board/index.js
@@ -1,7 +1,13 @@
 import React, { PureComponent } from 'react'
 
 import Row from './Row'
-import { mergeIntoStack, overlays } from '../../../overlays'
+import {
+  mergeIntoStack,
+  overlays,
+  rotateLeft,
+  rotateRight,
+  wouldCollide
+} from '../../../overlays'
 import './index.css'
 
 // Tetromino shapes are I O T L J Z S
@@ -32,10 +38,20 @@ class Board extends PureComponent {
     ],
     // tetromino: null,
     tetromino: {
-      overlay: overlays.S,
+      overlay: overlays.I,
       bottom: 1,
       left: 5
     }
+  }
+
+  handleKeyDown = this.handleKeyDown.bind(this)
+
+  componentDidMount () {
+    document.addEventListener('keydown', this.handleKeyDown)
+  }
+
+  componentWillUnmount () {
+    document.removeEventListener('keydown', this.handleKeyDown)
   }
 
   render () {
@@ -48,6 +64,31 @@ class Board extends PureComponent {
         </div>
       </div>
     )
+  }
+
+  handleKeyDown (e) {
+    const { stack } = this.state
+    let { tetromino, tetromino: { bottom, left, overlay } } = this.state
+
+    const code = e.code
+    if (code === 'ArrowLeft') {
+      left -= 1
+    } else if (code === 'ArrowRight') {
+      left += 1
+    } else if (code === 'ArrowDown') {
+      bottom += 1
+    } else if (code === 'ArrowUp' || code === 'AltLeft') {
+      overlay = rotateRight(overlay)
+    } else if (code === 'ControlLeft') {
+      overlay = rotateLeft(overlay)
+    } else {
+      return
+    }
+
+    tetromino = { ...tetromino, bottom, left, overlay }
+    if (!wouldCollide(tetromino, stack)) {
+      this.setState({ tetromino })
+    }
   }
 }
 
