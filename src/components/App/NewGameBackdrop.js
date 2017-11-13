4804370e846436cbe88f743c40548f5b543c6c76
Vianney Carel <vianney.carel@gmail.com>
2017-11-14 00:54:32 +0100

    Add game statuses so to start / pause / end game

---
+++ src/components/App/NewGameBackdrop.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NewGameBackdrop = () => (
+  <div className='backdrop'>
+    <div className='text'>
+      <h3>New Game</h3>
+      <p className='blink'>&lt;Press any key&gt;</p>
+    </div>
+  </div>
+)
+
+export default NewGameBackdrop
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 import Board from './Board'
+import NewGameBackdrop from './NewGameBackdrop'
 import {
   mergeIntoStack,
   overlays,
@@ -10,41 +11,39 @@ import {
 } from '../../overlays'
 import './index.css'
 
+// Board size is 18x12
+const initialState = {
+  stack: [
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', 'O', 'O', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'I'],
+    ['J', ' ', 'O', 'O', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'I'],
+    ['J', ' ', 'Z', 'Z', 'T', 'T', 'T', ' ', 'S', 'S', ' ', 'I'],
+    ['J', 'J', ' ', 'Z', 'Z', 'T', ' ', 'S', 'S', 'L', 'L', 'I']
+  ],
+  tetromino: null,
+  gameStatus: 'new',
+  level: null
+}
+
 class App extends Component {
-  // Board size is 18x12
-  state = {
-    // stack: Array(18).fill(Array(12).fill(' ')),
-    stack: [
-      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      [' ', ' ', 'O', 'O', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'I'],
-      ['J', 'J', 'O', 'O', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'I'],
-      [' ', 'J', 'Z', 'Z', 'T', 'T', 'T', ' ', 'S', 'S', ' ', 'I'],
-      ['T', 'J', ' ', 'Z', 'Z', 'T', ' ', 'S', 'S', 'T', ' ', 'I']
-    ],
-    // tetromino: null,
-    tetromino: {
-      overlay: overlays.L,
-      bottom: 1,
-      left: 5
-    }
-  }
+  state = initialState
 
   handleKeyDown = this.handleKeyDown.bind(this)
 
@@ -57,7 +56,7 @@ class App extends Component {
   }
 
   render () {
-    const { tetromino, stack } = this.state
+    const { tetromino, stack, gameStatus } = this.state
     return (
       <div className='app'>
         <div className='left pane' />
@@ -65,12 +64,7 @@ class App extends Component {
         <div className='middle pane'>
           <Board stack={tetromino ? mergeIntoStack(tetromino, stack) : stack} />
 
-          <div className='backdrop'>
-            <div className='text'>
-              <h3>New Game</h3>
-              <p className='blink'>&lt;Press any key&gt;</p>
-            </div>
-          </div>
+          {gameStatus === 'new' && <NewGameBackdrop />}
         </div>
 
         <div className='right pane' />
@@ -80,35 +74,57 @@ class App extends Component {
 
   handleKeyDown (e) {
     const code = e.code
+    const key = e.key
     const shiftKey = e.shiftKey
-    const { stack } = this.state
-    let { tetromino, tetromino: { bottom, left, overlay } } = this.state
 
-    if (code === 'ArrowLeft') {
-      left--
-    } else if (code === 'ArrowRight') {
-      left++
-    } else if (code === 'Enter' && !shiftKey) {
-      bottom++
-    } else if (code === 'ArrowUp' || code === 'End') {
-      left += Math.floor(overlay[0].length / 2)
-      overlay = rotateRight(overlay)
-      left -= Math.floor(overlay[0].length / 2)
-    } else if (code === 'ArrowDown' || code === 'Home') {
-      left += Math.floor(overlay[0].length / 2)
-      overlay = rotateLeft(overlay)
-      left -= Math.floor(overlay[0].length / 2)
-    } else if (code === 'Space' || (code === 'Enter' && shiftKey)) {
-      while (!wouldCollide({ ...tetromino, bottom: ++bottom }, stack)) {}
-      bottom--
-    } else {
-      return
+    if (this.state.gameStatus === 'new' && key.length === 1) {
+      // Alpha-numeric keys have a key length of 1
+      this.setState({ gameStatus: 'started' })
     }
 
-    tetromino = { ...tetromino, bottom, left, overlay }
-    if (!wouldCollide(tetromino, stack)) {
-      this.setState({ tetromino })
+    if (this.state.gameStatus === 'playing') {
+      const { stack } = this.state
+      let { tetromino, tetromino: { bottom, left, overlay } } = this.state
+
+      if (code === 'ArrowLeft') {
+        left--
+      } else if (code === 'ArrowRight') {
+        left++
+      } else if (code === 'Enter' && !shiftKey) {
+        bottom++
+      } else if (code === 'ArrowUp' || code === 'End') {
+        left += Math.floor(overlay[0].length / 2)
+        overlay = rotateRight(overlay)
+        left -= Math.floor(overlay[0].length / 2)
+      } else if (code === 'ArrowDown' || code === 'Home') {
+        left += Math.floor(overlay[0].length / 2)
+        overlay = rotateLeft(overlay)
+        left -= Math.floor(overlay[0].length / 2)
+      } else if (code === 'Space' || (code === 'Enter' && shiftKey)) {
+        while (!wouldCollide({ ...tetromino, bottom: ++bottom }, stack)) {}
+        bottom--
+      } else {
+        return
+      }
+
+      tetromino = { ...tetromino, bottom, left, overlay }
+      if (!wouldCollide(tetromino, stack)) {
+        this.setState({ tetromino })
+      }
     }
+  }
+
+  startNewGame () {
+    this.setState({
+      stack: Array(18).fill(Array(12).fill(' ')), // Board size is 18x12
+      tetromino: {
+        overlay: overlays.L,
+        bottom: 1,
+        left: 5
+      },
+      gameStatus: 'started',
+      level: 0
+    })
   }
 }
 
