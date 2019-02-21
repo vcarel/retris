e90fd8f24a28c7fbf3ad56fc5a63d6e6784f9ea5
Vianney Carel <vianney.carel@gmail.com>
2019-02-21 11:36:34 +0100

    v0.1.5

---
+++ docs/precache-manifest.4989a79a0c36428d41002295cf8416a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c60bceb0bd0f908ae65",
+    "url": "/retris/static/js/runtime~main.6c60bceb.js"
+  },
+  {
+    "revision": "6405fd42c1f6d8035a76",
+    "url": "/retris/static/js/main.6405fd42.chunk.js"
+  },
+  {
+    "revision": "e0596429b7a82665d63a",
+    "url": "/retris/static/js/2.e0596429.chunk.js"
+  },
+  {
+    "revision": "6405fd42c1f6d8035a76",
+    "url": "/retris/static/css/main.9d68ac35.chunk.css"
+  },
+  {
+    "revision": "419156d6e26ee8e2cc0e4a3df7eee09d",
+    "url": "/retris/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e03a44783e2336f14cc5ff1a1ae38d8d.js"
+  "/retris/precache-manifest.4989a79a0c36428d41002295cf8416a3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/retris/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
