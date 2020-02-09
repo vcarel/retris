585161b888778e3671f9d3f788038ebbf7b75e1e
Vianney Carel <vianney.carel@gmail.com>
2020-02-09 22:26:58 +0100

    Updates

---
+++ precache-manifest.8892c3e1ac96db1bd1982bb73d70bd38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e39063f07ab06bff7de20f0021815435",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aaeaa6e842ce8cf51bc4",
+    "url": "/static/css/main.289b0b96.chunk.css"
+  },
+  {
+    "revision": "44417aa6c262a74e28a3",
+    "url": "/static/js/2.746846e0.chunk.js"
+  },
+  {
+    "revision": "959150e688866e3de05cb62dc4dcd7db",
+    "url": "/static/js/2.746846e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaeaa6e842ce8cf51bc4",
+    "url": "/static/js/main.99f82ff5.chunk.js"
+  },
+  {
+    "revision": "9bbef3c0e9091df6cb6c",
+    "url": "/static/js/runtime-main.5434dec4.js"
+  }
+]);
